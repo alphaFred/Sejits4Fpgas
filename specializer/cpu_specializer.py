100bd81adb974afac35ec13ed35f8faea2fe9baf
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2015-11-17 14:24:24 +0100

    target specializer

---
+++ specializer/cpu_specializer.py
@@ -0,0 +1,10 @@
+__author__ = 'philipp'
+
+
+class X86_Specializer(object):
+    def __init__(self):
+        pass
+
+class ARM_Specializer(object):
+    def __init__(self):
+        pass
---
+++ specializer/gpu_specializer.py
@@ -0,0 +1,6 @@
+__author__ = 'philipp'
+
+
+class CUDA_Specializer(object):
+    def __init__(self):
+        pass
