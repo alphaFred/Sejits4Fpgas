4d8d352fc0ac0724a3d575ea1f47bfce516f8758
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2015-11-17 14:52:18 +0100

    Updated ZYNQ_Specializer name; added docstring stubs

--- specializer/fpga_specializer.py
+++ specializer/fpga_specializer.py
@@ -11,8 +11,10 @@ from specializer.utils.dsl_ast_transformer import DslAstData
 from specializer.utils.dsl_ast_transformer import DslAstTransformer
 
 
-class Zynq_Specializer(object):
+class ZYNQ_Specializer(object):
+    """ docstring for ZYNQ_Specializer """
     def __init__(self, args, func):
+        """ docstring for __init__ """
         # TODO: remove ALL decorators! -> multiple decorators etc.
         func_src = "\n".join(inspect.getsource(func).splitlines()[1:])
         func_ast = ast.parse(func_src.lstrip())
@@ -24,6 +26,7 @@ class Zynq_Specializer(object):
     #   HELPER METHODS                                                        #
     # ======================================================================= #
     def run(self):
+        """ docstring for run """
         ast_transformer = DslAstTransformer(self.func_ast_data)
         # DEBUG
         print "\nKERNEL TRANSFORMED\n"
