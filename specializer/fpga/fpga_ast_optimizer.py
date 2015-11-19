90301f316183d13e2b78b9d821ca475417bc9fc1
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2015-11-19 18:46:03 +0100

    started dag2ast transformer
    pprinter is just temporary; output formatting has to be included into
    sejits_ctree

--- specializer/fpga/fpga_ast_optimizer.py
+++ specializer/fpga/fpga_ast_optimizer.py
@@ -1,13 +1,217 @@
 __author__ = 'philipp'
 
+
 import ast
+import os
+
+from subprocess import Popen, PIPE
+from collections import namedtuple
+
+
+Pixel = namedtuple('Pixel',['mode','byte_size'])
+PixelMask = namedtuple('PixelMask',['height','width','pattern','pixel'])
+
+PipelineObj = namedtuple('PipelineObj',['id','consumer','producer', 'next_id','next'])
+
+def pprinter(flow_data, data_flow_graph):
+    formats = {"ImageFilter": ', style=filled, fillcolor="#00EB5E"',
+               "ImagePointOp":', style=filled, fillcolor="#C2FF66"',
+               "TempAssign":  ', style=filled, fillcolor="#66C2FF"',
+               "OutAssign":   ', style=filled, fillcolor="#6675FF"',
+               "InImageObj":  ', style=filled, fillcolor="#FFF066"',
+               "OutImageObj": ', style=filled, fillcolor="#FFA366"'
+               }
+    # generate dot
+    graphviz_opts = ['rankdir="LR"']
+    dot_text = "digraph G {\n %s \n" % str.join("\n", graphviz_opts)
+    print "\n\n"
+    for f_obj in data_flow_graph:
+        flow_obj = flow_data[f_obj]
+        print flow_obj
+        dot_text += '%s ["label"="%s"%s];\n' %(flow_obj.next_id, flow_obj.id, formats.get(flow_obj.id, ""))
+        for next_obj in flow_obj.next:
+            dot_text += '%s -> %s ["label"="%s"];\n' %(flow_obj.next_id, next_obj.next_id,str(flow_obj.producer.__class__.__name__ + "->" + next_obj.consumer.__class__.__name__))
+    dot_text += "}"
+    #
+    print dot_text
+    #
+    dot_args = ['dot'] + ['-T', 'png']
+    p = Popen(dot_args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate(dot_text.encode('utf-8'))
+    #
+    print "stderr:", stderr
+    #
+    with open("./images/"+"data_flow_graph.png", "wb") as f:
+                f.write(stdout)
+
 
 class FpgaAstOptimizer(ast.NodeTransformer):
     """ docstring for FpgaAstOptimizer """
-    def __init__(self, arg):
+    def __init__(self, ast, dsl_classes):
         """ docstring for __init__ """
+        globals().update(dsl_classes)
         super(FpgaAstOptimizer, self).__init__()
-        self.arg = arg
+        self.ast = ast
+        self.identifiers = dict()
+        self.data_flow = dict()
+        self.flow_id = 0
+        self.data_flow_graph = []
+
+    def run(self):
+        bla = FIA_Kernel_Linearizer()
+        return bla.run(self.ast)
+        self.visit(self.ast)
+        print self.data_flow
+        print self.data_flow_graph
+        pprinter(self.data_flow, self.data_flow_graph)
+
+    def visit_OutAssign(self, node):
+        prev = self.visit(node.value)
+        if prev is not None:
+            prev.next.append(self.visit(node.var))
+            self.data_flow[prev.next_id] = prev
+            self.data_flow_graph.append(prev.next_id)
+            #
+            next = self.visit(node.var)
+            self.data_flow[next.next_id] = next
+            self.data_flow_graph.append(next.next_id)
+            """
+            #
+            self.data_flow_graph.append(prev)
+            next = self.visit(node.var)
+            if next is not None:
+                self.data_flow_graph.append(next)
+            """
+
+    def visit_TempAssign(self, node):
+        prev = self.visit(node.value)
+        if prev is not None:
+            self.data_flow_graph.append(prev)
+            next = PipelineObj(id=node.var.name,
+                               consumer=None,
+                               producer=prev.producer)
+            self.identifiers[node.var.name] = next
+
+    def visit_ImagePointOp(self, node):
+        prev = self.visit(node.value)
+        if prev is not None:
+            this = PipelineObj(id='ImagePointOp',
+                               consumer=Pixel(mode='RGB', byte_size=3),
+                               producer=Pixel(mode='RGB', byte_size=3),
+                               next_id=hash(node),
+                               next=[])
+            prev.next.append(this)
+            self.data_flow[prev.next_id] = prev
+            self.data_flow_graph.append(prev.next_id)
+            return this
+            #
+        """
+        prev = self.visit(node.target)
+        if prev is not None:
+            self.data_flow_graph.append(prev)
+            pipeObj = PipelineObj(id='ImagePointOp',
+                                  consumer=Pixel(mode='RGB', byte_size=3),
+                                  producer=Pixel(mode='RGB', byte_size=3))
+            return pipeObj
+        else:
+            return None
+        """
+
+    def visit_ImageFilter(self, node):
+        prev = self.visit(node.target)
+        if prev is not None:
+            this = PipelineObj(id='ImagePointOp',
+                               consumer=PixelMask(height=node.filter.size.n,
+                                                  width=node.filter.size.n,
+                                                  pattern=[1,1,1,1,0,1,1,1,1],
+                                                  pixel=Pixel(mode='RGB', byte_size=3)),
+                               producer=Pixel(mode='RGB', byte_size=3),
+                               next_id=hash(node),
+                               next=[])
+            prev.next.append(this)
+            self.data_flow[prev.next_id] = prev
+            self.data_flow_graph.append(prev.next_id)
+            return this
+        """
+        prev = self.visit(node.target)
+        if prev is not None:
+            self.data_flow_graph.append(prev)
+            # PixelMask = namedtuple('PixelMask',['height','width','pattern','pixel'])
+            cons = PixelMask(height=node.filter.size.n,
+                             width=node.filter.size.n,
+                             pattern=[1,1,1,1,0,1,1,1,1],
+                             pixel=Pixel(mode='RGB', byte_size=3))
+            prod = Pixel(mode='RGB', byte_size=3)
+            #
+            pipeObj = PipelineObj(id='ImageFilter',
+                                  consumer=cons,
+                                  producer=prod)
+            self.data_flow_graph.append(pipeObj)
+            #
+            return pipeObj
+        else:
+            return None
+        """
+
+    def visit_InImageObj(self, node):
+        prod = Pixel(mode=node.mode, byte_size=3)
+        ret = PipelineObj(id="InImageObj",
+                          consumer=None,
+                          producer=prod,
+                          next_id=hash(node),
+                          next=[])
+        self.flow_id += 1
+        return ret
+
+    def visit_OutImageObj(self, node):
+        cons = Pixel(mode=node.mode, byte_size=3)
+        ret = PipelineObj(id="OutImageObj",
+                          consumer=cons,
+                          producer=None,
+                          next_id=hash(node),
+                          next=[])
+        self.flow_id += 1
+        return ret
+
+    def visit_Identifier(self, node):
+        return self.identifiers[node.name]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class FIA_Kernel_Optimizer(ast.NodeTransformer):
@@ -34,6 +238,47 @@ class FIA_Kernel_Optimizer(ast.NodeTransformer):
         kernel_unrolled = self.kernel_converter.run(kernel_linearized)
         return kernel_unrolled
 
+class DFImageFilter(ast.AST):
+    """docstring for Data_Buffer"""
+    _attributes = ('lineno', 'col_offset')
+    _fields = ['target']
+    # _attributes
+    col_offset = None
+    lineno = None
+    # _fields
+    target = None
+
+    def label(self):
+        return ""
+
+class DFBinOp(ast.AST):
+    """docstring for Data_Buffer"""
+    _attributes = ('lineno', 'col_offset')
+    _fields = ['left', 'right']
+    # _attributes
+    col_offset = None
+    lineno = None
+    # _fields
+    left = None
+    right = None
+
+    def label(self):
+        return ""
+
+class DFOutAssign(ast.AST):
+    """docstring for Data_Buffer"""
+    _attributes = ('lineno', 'col_offset')
+    _fields = ['var', 'value']
+    # _attributes
+    col_offset = None
+    lineno = None
+    # _fields
+    var = None
+    value = None
+
+    def label(self):
+        return ""
+
 
 class FIA_Kernel_Linearizer(ast.NodeTransformer):
     def __init__(self):
@@ -45,9 +290,9 @@ class FIA_Kernel_Linearizer(ast.NodeTransformer):
         return self.visit(ast)
 
     def visit_TempAssign(self, node):
-        if node.target.name not in self.local_vars:
+        if node.var.name not in self.local_vars:
             node_value = self.visit(node.value)
-            self.local_vars[node.target.name] = node_value
+            self.local_vars[node.var.name] = node_value
         else:
             assert False, "{0} has been already used as assignment target!".format(node.target.name)
 
@@ -56,11 +301,16 @@ class FIA_Kernel_Linearizer(ast.NodeTransformer):
             return self.local_vars[node.name]
         else:
             return node
-            #assert False, "{0} undefined!".format(node.name)
+
+    def visit_ImageFilter(self, node):
+        return DFImageFilter(filter=None, target=node.target)
+
+    def visit_BinOp(self, node):
+        return DFBinOp(left=self.visit(node.left), right=self.visit(node.right))
 
     def visit_OutAssign(self, node):
         node_value = self.visit(node.value)
-        return OutAssign(target=node.target, value=node_value)
+        return DFOutAssign(var=node.var, value=node_value,)
 
 
 class Data_Widening(ast.AST):
