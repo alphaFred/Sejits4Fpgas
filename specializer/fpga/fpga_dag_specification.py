8adb624ee9cb86522ee4ea81805f3873af8a5648
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2015-11-25 16:38:58 +0100

    created specifiaction with ned DAG classes

--- specializer/fpga/fpga_dag_specification.py
+++ specializer/fpga/fpga_dag_specification.py
@@ -8,11 +8,14 @@ class DagNode(ast.AST):
 
     """ Describes base class for Dag-Nodes. """
 
+    iput_intf = None
+    oput_intf = None
     prev = None
     next = None
     d = None
+    d_prev = None
 
-    def __init__(self, prev=[], next=[], d=int):
+    def __init__(self, iput_intf, oput_intf, prev=[], next=[], d=int, d_prev=int):
         """
         Initialize class variables.
 
@@ -20,17 +23,72 @@ class DagNode(ast.AST):
         next: List of subsequent Dag-Nodes
         d:    Propagation delay of Dag-Node
         """
+        self._attributes = ('iput_intf', 'oput_intf', 'prev', 'next', 'd', 'd_prev')
         self._fields = ('prev', 'next')
         super(DagNode, self).__init__(lineno=None, col_offset=None)
+        self.iput_intf = iput_intf
+        self.oput_intf = oput_intf
         self.prev = prev
         self.next = next
         self.d = d
+        self.d_prev = d_prev
 
 
 class DagNodeInImage(DagNode):
 
     """ Describes an InImage Dag-Node. """
 
-    def __init__(self):
-        """ Initalize class variables. """
-        pass
+    def __init__(self, **kwargs):
+        """ Initalize base class and class variables. """
+        super(DagNodeInImage, self).__init__(**kwargs)
+
+
+class DagNodeOutImage(DagNode):
+
+    """ Describes an OutImage Dag-Node. """
+
+    def __init__(self, **kwargs):
+        """ Initalize base class and class variables. """
+        super(DagNodeOutImage, self).__init__(**kwargs)
+
+
+class DagNodeImgFilter(DagNode):
+
+    """ Describes an Image Filter Dag-Node. """
+
+    def __init__(self, **kwargs):
+        """ Initalize base class and class variables. """
+        super(DagNodeImgFilter, self).__init__(**kwargs)
+
+
+class DagNodeImgPointOp(DagNode):
+
+    """ Describes an Image Point Operator Dag-Node. """
+
+    def __init__(self, **kwargs):
+        """ Initalize base class and class variables. """
+        super(DagNodeImgPointOp, self).__init__(**kwargs)
+
+
+class DagNodeBinOp(DagNode):
+
+    """ Describes an Binary Operator Dag-Node. """
+
+    def __init__(self, **kwargs):
+        """ Initialize base class and class variables. """
+        super(DagNodeBinOp, self).__init__(**kwargs)
+
+
+class DagNodeDReg(DagNode):
+
+    """ Describes an Delay Register Dag-Node. """
+
+    def __init__(self, **kwargs):
+        """ Initalize base class and class variables. """
+        super(DagNodeDReg, self).__init__(**kwargs)
+
+dag_node_dict = {"DagNodeInImage": DagNodeInImage,
+                 "DagNodeOutImage": DagNodeOutImage,
+                 "DagNodeImgFilter": DagNodeImgFilter,
+                 "DagNodeImgPointOp": DagNodeImgPointOp,
+                 "DagNodeBinOp": DagNodeBinOp}
