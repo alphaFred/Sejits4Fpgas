f119088b870bc188b5533cffaf8d172d333f9779
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-01-26 12:03:17 +0100

    made TransformationError module wide

--- sejits_ctree/vhdl/__init__.py
+++ sejits_ctree/vhdl/__init__.py
@@ -1,5 +1,21 @@
 
-STDLIBS = ["library ieee", "use ieee.std_logic_1164.all"]
+STDLIBS = ["ieee", "ieee.std_logic_1164.all"]
 
 from sejits_ctree.vhdl.scikit_image.transformers import SobelTransformer
 USERTRANSFORMERS = [SobelTransformer]
+
+
+class TransformationError(Exception):
+
+    """
+    Exception that caused transformation not to occur.
+
+    Attributes:
+      msg -- the message/explanation to the user
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
