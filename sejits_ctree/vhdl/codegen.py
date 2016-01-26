2b31bf5b90be590449f175ef2efeee0aeee87d0c
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-01-26 12:01:40 +0100

    Implemented vhdlcodegen

--- sejits_ctree/vhdl/codegen.py
+++ sejits_ctree/vhdl/codegen.py
@@ -4,6 +4,8 @@ __author__ = 'philipp ebensberger'
 
 import ast
 
+from nodes import VhdlType
+
 
 VHDLMODULE = """{libraries}\n\n
 {entity}\n
@@ -12,7 +14,8 @@ VHDLMODULE = """{libraries}\n\n
 ARCHITECTURE = """architecture {architecture_name} of {entity_name} is
 {architecture_declarations}
 begin
-{architecture_instructions}\
+{architecture_instructions}
+{entity_return} <= {architecture_return};
 end {architecture_name};"""
 
 ENTITY = """entity {entity_name} is\
@@ -22,9 +25,9 @@ end {entity_name};"""
 
 SIGNAL = "signal {signal_name} : {signal_type};"
 
-CONSTANT = "constant {const_name} : {const_type} = {const_value}"
+CONSTANT = "constant {const_name} : {const_type} := {const_value};"
 
-COMPONENT = """{instance_name} : {component_name}\
+COMPONENT = """{instance_name} : {component_name} entity {component_lib}\
 {generic_map}\
 {port_map};
 """
@@ -32,16 +35,23 @@ COMPONENT = """{instance_name} : {component_name}\
 
 class VhdlCodeGen(ast.NodeVisitor):
 
-    def __init__(self, indent=0):
+    active_entity = None
+    active_arch = None
+
+    def __init__(self, indent=4):
         self._indent = indent
 
     def _tab(self):
         """return correct spaces if tab found"""
-        return "    " * self._indent
+        return " " * self._indent
 
     def visit_VhdlFile(self, node):
         """ Generate Vhdl description of VhdlFile. """
-        libraries = "\n".join(node.libs)
+        libraries = "library " + "\nuse ".join(node.libs)
+        #
+        self.active_entity = node.body[0]
+        self.active_arch = node.body[1]
+        #
         entity = self.visit(node.body[0])
         architecture = self.visit(node.body[1])
         return VHDLMODULE.format(libraries=libraries,
@@ -66,21 +76,17 @@ class VhdlCodeGen(ast.NodeVisitor):
         #
         declarations = self._tab() +\
             join_statement.join([self.visit(sig) for sig in node.signals])
-        instructions = "\n".join([self.visit(comp) for comp in node.components])
+        instructions = "\n".join([self.visit(comp) for comp in node.body])
+        #
+        ret_port_name = self.active_entity.out_port.name
+        ret_sig_name = self.active_entity.out_port.value.name
         # return vhdl description of architecture
         return ARCHITECTURE.format(architecture_name=node.architecture_name,
                                    entity_name=node.entity_name,
                                    architecture_declarations=declarations,
-                                   architecture_instructions=instructions)
-
-    def visit_VhdlReturn(self, node):
-        generic_map = ""
-        port_map = self._port_map([node.in_port, node.out_port])
-        #
-        return COMPONENT.format(instance_name=node.instance_name,
-                                component_name=node.name,
-                                generic_map=generic_map,
-                                port_map=port_map)
+                                   architecture_instructions=instructions,
+                                   entity_return=ret_port_name,
+                                   architecture_return=ret_sig_name)
 
     def visit_Component(self, node):
         if node.generics != []:
@@ -92,39 +98,87 @@ class VhdlCodeGen(ast.NodeVisitor):
         #
         return COMPONENT.format(instance_name=node.instance_name,
                                 component_name=node.name,
+                                component_lib=node.lib_name,
                                 generic_map=generic_map,
                                 port_map=port_map)
 
     def visit_BinaryOp(self, node):
         generic_map = self._generic_map([node.op])
-        port_map = self._port_map([node.left_port,
-                                   node.right_port,
-                                   node.out_port])
+        _in_ports = [self.type_cast(VhdlType.VhdlStdLogicVector, port)
+                     for port in [node.left_port, node.right_port]]
+        port_map = self._port_map(_in_ports + node.in_ports[2:] + [node.out_port])
         #
         return COMPONENT.format(instance_name=node.instance_name,
                                 component_name=node.name,
+                                component_lib=node.lib_name,
                                 generic_map=generic_map,
                                 port_map=port_map)
 
+    def type_cast(self, out_type, port):
+        castable = (VhdlType.VhdlSigned,
+                    VhdlType.VhdlUnsigned,
+                    VhdlType.VhdlStdLogicVector)
+
+        class cast(object):
+
+            class obj_name(object):
+                def __init__(self, out_type, name):
+                    self.name = out_type.vhdl_type + "(" + name + ")"
+
+            def __init__(self, out_type, port):
+                self.name = port.name
+                self.value = self.obj_name(out_type, port.value.name)
+        #
+        if type(port.value.vhdl_type) is out_type:
+            return port
+        else:
+            if type(port.value.vhdl_type) in castable:
+                return cast(out_type, port)
+
     def visit_UnaryOp(self, node):
         generic_map = self._generic_map([node.op])
-        port_map = self._port_map([node.in_port, node.out_port])
+        port_map = self._port_map(node.in_ports + [node.out_port])
         #
         return COMPONENT.format(instance_name=node.instance_name,
                                 component_name=node.name,
+                                component_lib=node.lib_name,
                                 generic_map=generic_map,
                                 port_map=port_map)
 
     def visit_Signal(self, node):
         """ Return signal description for architecture declaration. """
         return SIGNAL.format(signal_name=node.name,
-                             signal_type=node.vhdl_type)
+                             signal_type=self.visit(node.vhdl_type))
 
     def visit_Constant(self, node):
         """ Return constant description for architecture declaration. """
         return CONSTANT.format(const_name=node.name,
-                               const_type=node.vhdl_type,
+                               const_type=self.visit(node.vhdl_type),
                                const_value=node.value)
+
+    def visit_VhdlString(self, node):
+        return node.vhdl_type
+
+    def visit_VhdlArray(self, node):
+        if node.generated:
+            raise Exception("Generated arrays currently unsupported")
+        else:
+            return node.type_def
+
+    def visit_VhdlUnsigned(self, node):
+        return node.vhdl_type
+
+    def visit_VhdlSigned(self, node):
+        return node.vhdl_type
+
+    def visit_VhdlStdLogicVector(self, node):
+        if node.default:
+            if len(node.default) == 1:
+                return node.vhdl_type + " := (others => " + "'" + node.default[0] + "'" + ")"
+            else:
+                return node.vhdl_type + ' := "' + "".join(node.default) + '"'
+        else:
+            return node.vhdl_type
 
     # ======================================================================= #
     # HELPER METHODS FOR ENTITY
@@ -132,7 +186,7 @@ class VhdlCodeGen(ast.NodeVisitor):
     def _generic_block(self, generics):
         block_indent = " " * len("generic(")
         #
-        generic_block = [g.name + " : " + str(g.vhdl_type) for g in generics]
+        generic_block = [g.name + " : " + self.visit(g.vhdl_type) for g in generics]
         join_statement = ";\n" + self._tab() + block_indent
         #
         s = "\n"
@@ -144,8 +198,7 @@ class VhdlCodeGen(ast.NodeVisitor):
     def _port_block(self, ports):
         block_indent = " " * len("port(")
         #
-        port_block = [p.name + " : " + p.direction + " " +
-                      str(p.value.vhdl_type) for p in ports]
+        port_block = [p.name + " : " + p.direction + " " + str(p.value.vhdl_type) for p in ports]
         join_statement = ";\n" + self._tab() + block_indent
         #
         s = "\n"
