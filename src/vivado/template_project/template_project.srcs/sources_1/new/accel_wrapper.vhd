e6b7d1c2b3f009e88eea036478b8653f1fa39c70
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-04-15 14:01:32 +0200

    updated accel_wrapper

--- src/vivado/template_project/template_project.srcs/sources_1/new/accel_wrapper.vhd
+++ src/vivado/template_project/template_project.srcs/sources_1/new/accel_wrapper.vhd
@@ -9,20 +9,15 @@ entity accel_wrapper is
          RST : in std_logic;
          VALID_IN : in std_logic;
          m_axis_mm2s_tdata : in std_logic_vector(31 downto 0);
-         m_axis_mm2s_tkeep : in std_logic_vector(3 downto 0);
          m_axis_mm2s_tlast : in std_logic;
          m_axis_mm2s_tready : in std_logic;
          VALID_OUT : out std_logic;
          s_axis_s2mm_tdata : out std_logic_vector(31 downto 0);
-         s_axis_s2mm_tkeep : out std_logic_vector(3 downto 0);
          s_axis_s2mm_tlast : out std_logic;
-         s_axis_s2mm_tready : out std_logic);                
-end accel_wrapper;
+         s_axis_s2mm_tready : out std_logic);                end accel_wrapper;
 
-architecture BEHAVE of accel_wrapper is                          
-    signal VhdlComponent_VALID_OUT_0 : std_logic;
-    signal ret_tdata : std_logic_vector(7 downto 0);                      
-begin                          
+architecture BEHAVE of accel_wrapper is                              signal VhdlComponent_VALID_OUT_0 : std_logic;
+    signal ret_tdata : std_logic_vector(7 downto 0);                      begin                          
 VhdlComponent : entity work.apply                       
     port map(CLK => CLK,
              RST => RST,
@@ -35,5 +30,4 @@ VhdlComponent : entity work.apply
 VALID_OUT <= VhdlComponent_VALID_OUT_0;
 s_axis_s2mm_tdata <= (31 downto 8 => '0') & ret_tdata;
 s_axis_s2mm_tlast <= m_axis_mm2s_tlast;
-s_axis_s2mm_tready <= m_axis_mm2s_tready;                      
-end BEHAVE;
+s_axis_s2mm_tready <= m_axis_mm2s_tready;                      end BEHAVE;
