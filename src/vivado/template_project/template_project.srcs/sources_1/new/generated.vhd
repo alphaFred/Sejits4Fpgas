53e142d62cdf876612755149d4d53dab6b0b9725
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-04-23 10:27:43 +0200

    auto update of vivado files; updated test function

--- src/vivado/template_project/template_project.srcs/sources_1/new/generated.vhd
+++ src/vivado/template_project/template_project.srcs/sources_1/new/generated.vhd
@@ -12,45 +12,31 @@ entity apply is
          VALID_OUT : out std_logic;
          MODULE_OUT : out std_logic_vector(31 downto 0));                end apply;
 
-architecture BEHAVE of apply is                              signal VhdlComponent_VALID_OUT_0 : std_logic;
-    signal VhdlComponent_OUT_0 : std_logic_vector(31 downto 0);
-    signal VhdlComponent_VALID_OUT_1 : std_logic;
-    signal VhdlComponent_OUT_1 : std_logic_vector(31 downto 0);
-    signal VhdlComponent_VALID_OUT_2 : std_logic;
-    signal VhdlComponent_OUT_2 : std_logic_vector(31 downto 0);                      begin                          
+architecture BEHAVE of apply is                              signal BB_SPLIT_VALID_OUT_0 : std_logic;
+    signal BB_SPLIT_OUT_0 : std_logic_vector(31 downto 0);
+    signal BB_CONVOLVE_VALID_OUT_0 : std_logic;
+    signal BB_CONVOLVE_OUT_0 : std_logic_vector(31 downto 0);                      begin                          
 VhdlComponent : entity work.split                       
     port map(CLK => CLK,
              RST => RST,
              VALID_IN => VALID_IN,
              DATA_IN => a,
              INDEX => to_unsigned(0, 8),
-             VALID_OUT => VhdlComponent_VALID_OUT_0,
-             DATA_OUT => VhdlComponent_OUT_0); 
+             VALID_OUT => BB_SPLIT_VALID_OUT_0,
+             DATA_OUT => BB_SPLIT_OUT_0); 
 
 VhdlComponent_1 : entity work.Convolve                       
-    generic map(FILTERMATRIX => (-1, 0, 1, -2, 0, 2, -1, 0, 1),
-                FILTER_SCALE => 16,
-                IMG_WIDTH => 315,
-                IMG_HEIGHT => 300)                       
-    port map(CLK => CLK,
-             RST => RST,
-             VALID_IN => VhdlComponent_VALID_OUT_0,
-             DATA_IN => VhdlComponent_OUT_0,
-             VALID_OUT => VhdlComponent_VALID_OUT_1,
-             DATA_OUT => VhdlComponent_OUT_1); 
-
-VhdlComponent_2 : entity work.Convolve                       
     generic map(FILTERMATRIX => (1, 2, 1, 2, 4, 2, 1, 2, 1),
                 FILTER_SCALE => 16,
                 IMG_WIDTH => 315,
                 IMG_HEIGHT => 300)                       
     port map(CLK => CLK,
              RST => RST,
-             VALID_IN => VhdlComponent_VALID_OUT_1,
-             DATA_IN => VhdlComponent_OUT_1,
-             VALID_OUT => VhdlComponent_VALID_OUT_2,
-             DATA_OUT => VhdlComponent_OUT_2); 
+             VALID_IN => BB_SPLIT_VALID_OUT_0,
+             DATA_IN => BB_SPLIT_OUT_0,
+             VALID_OUT => BB_CONVOLVE_VALID_OUT_0,
+             DATA_OUT => BB_CONVOLVE_OUT_0); 
 
 -- RETURN
-VALID_OUT <= VhdlComponent_VALID_OUT_2;
-MODULE_OUT <= VhdlComponent_OUT_2;                      end BEHAVE;
+VALID_OUT <= BB_CONVOLVE_VALID_OUT_0;
+MODULE_OUT <= BB_CONVOLVE_OUT_0;                      end BEHAVE;
