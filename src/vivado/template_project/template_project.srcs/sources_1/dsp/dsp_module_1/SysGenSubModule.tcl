eb7fad7b019bd9e964273c959f71cb4b1fb0bc2d
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-04-27 19:38:09 +0200

    removed vivado template project

--- src/vivado/template_project/template_project.srcs/sources_1/dsp/dsp_module_1/SysGenSubModule.tcl
+++
@@ -1,5 +0,0 @@
-namespace eval ::xilinx::dsp::sysgensubmodule {
-set is_generating_sysgensubmodule {true}
-set submodule_name {dsp_module_1}
-}
-source ProjectGeneration.tcl -notrace
--- src/vivado/template_project/template_project.srcs/sources_1/new/generated.vhd
+++
@@ -1,28 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.the_filter_package.all;
-
-
-entity apply is                    
-    port(CLK : in std_logic;
-         RST : in std_logic;
-         VALID_IN : in std_logic;
-         a : in std_logic_vector(31 downto 0);
-         VALID_OUT : out std_logic;
-         MODULE_OUT : out std_logic_vector(31 downto 0));                end apply;
-
-architecture BEHAVE of apply is                              signal BB_ADD_VALID_OUT_0 : std_logic;
-    signal BB_ADD_OUT_0 : std_logic_vector(31 downto 0);                      begin                          
-VhdlComponent : entity work.AddBB                       
-    port map(CLK => CLK,
-             RST => RST,
-             VALID_IN => VALID_IN,
-             LEFT => std_logic_vector(to_signed(125, 32)),
-             RIGHT => a,
-             VALID_OUT => BB_ADD_VALID_OUT_0,
-             ADD_OUT => BB_ADD_OUT_0); 
-
--- RETURN
-VALID_OUT <= BB_ADD_VALID_OUT_0;
-MODULE_OUT <= BB_ADD_OUT_0;                      end BEHAVE;
