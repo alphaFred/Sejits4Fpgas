4c1e3e356650d0df13f636b79f196fb74b046122
Philipp Ebensberger <ebensberger@codesign99>
2016-05-04 19:58:34 +0200

    vivado file update

--- src/vivado/template_project/template_project.srcs/sources_1/new/accel_wrapper.vhd
+++ src/vivado/template_project/template_project.srcs/sources_1/new/accel_wrapper.vhd
@@ -14,10 +14,13 @@ entity accel_wrapper is
          VALID_OUT : out std_logic;
          s_axis_s2mm_tdata : out std_logic_vector(31 downto 0);
          s_axis_s2mm_tlast : out std_logic;
-         m_axis_mm2s_tready : out std_logic);                end accel_wrapper;
+         m_axis_mm2s_tready : out std_logic);                
+end accel_wrapper;
 
-architecture BEHAVE of accel_wrapper is                              signal GENERATED_VALID_OUT_0 : std_logic;
-    signal ret_tdata : std_logic_vector(31 downto 0);                      begin                          
+architecture BEHAVE of accel_wrapper is                          
+    signal GENERATED_VALID_OUT_0 : std_logic;
+    signal ret_tdata : std_logic_vector(31 downto 0);                      
+begin                          
 VhdlComponent : entity work.apply                       
     port map(CLK => CLK,
              RST => RST,
@@ -30,4 +33,5 @@ VhdlComponent : entity work.apply
 VALID_OUT <= GENERATED_VALID_OUT_0;
 s_axis_s2mm_tdata <= ret_tdata;
 s_axis_s2mm_tlast <= m_axis_mm2s_tlast;
-m_axis_mm2s_tready <= s_axis_s2mm_tready;                      end BEHAVE;
+m_axis_mm2s_tready <= s_axis_s2mm_tready;                      
+end BEHAVE;
