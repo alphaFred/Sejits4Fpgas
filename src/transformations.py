bc7651ee1757c670d36aa892f1fda5f7f162b646
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-05-11 16:28:59 +0200

    updated documentation

--- src/transformations.py
+++ src/transformations.py
@@ -32,63 +32,20 @@ UNARY_OP = namedtuple("UNARY_OP", ["i_args", "out_arg"])
 
 
 class VhdlBaseTransformer(object):
-    """This class docstring shows how to use sphinx and rst syntax
+    """Base Transformer for the VHDL Back-End.
 
-    The first line is brief explanation, which may be completed with
-    a longer one. For instance to discuss about its methods. The only
-    method here is :func:`function1`'s. The main idea is to document
-    the class and methods's arguments with
+        This transformer coordinates all subtransformer necessary to transform and CTree AST
+        into an valid VHDL IR AST.
 
-    - **parameters**, **types**, **return** and **return types**::
-
-          :param arg1: description
-          :param arg2: description
-          :type arg1: type description
-          :type arg1: type description
-          :return: return description
-          :rtype: the return type description
-
-    - and to provide sections such as **Example** using the double commas syntax::
-
-          :Example:
-
-          followed by a blank line !
-
-      which appears as follow:
-
-      :Example:
-
-      followed by a blank line
-
-    - Finally special sections such as **See Also**, **Warnings**, **Notes**
-      use the sphinx syntax (*paragraph directives*)::
-
-          .. seealso:: blabla
-          .. warnings also:: blabla
-          .. note:: blabla
-          .. todo:: blabla
-
-    .. note::
-        There are many other Info fields but they may be redundant:
-            * param, parameter, arg, argument, key, keyword: Description of a
-              parameter.
-            * type: Type of a parameter.
-            * raises, raise, except, exception: That (and when) a specific
-              exception is raised.
-            * var, ivar, cvar: Description of a variable.
-            * returns, return: Description of the return value.
-            * rtype: Return type.
-
-    .. note::
-        There are many other directives such as versionadded, versionchanged,
-        rubric, centered, ... See the sphinx documentation for more details.
-
-    Here below is the results of the :func:`function1` docstring.
-
+        The currently implemented transformers are:
+            - VhdlKwdTransformer
+            - VhdlIRTransformer
+            - VhdlGraphTransformer
+            - VhdlPortTransformer
     """
 
     def __init__(self, ipt_params, lifted_functions):
-        """Initialize transform parameters."""
+        """Initialize transforer parameters."""
         self.ipt_params = ipt_params
         self.lifted_functions = lifted_functions
 
