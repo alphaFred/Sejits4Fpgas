ce7c0d5a0c44715eb71b4b53045d683c513dc06b
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-05-12 10:57:52 +0200

    auto update of vivado files

--- src/vivado/template_project/template_project.srcs/sources_1/new/generated.vhd
+++
@@ -1,37 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.the_filter_package.all;
-
-
-entity apply is                    
-    port(CLK : in std_logic;
-         RST : in std_logic;
-         VALID_IN : in std_logic;
-         READY_IN : in std_logic;
-         img : in std_logic_vector(31 downto 0);
-         VALID_OUT : out std_logic;
-         READY_OUT : out std_logic;
-         MODULE_OUT : out std_logic_vector(31 downto 0));                end apply;
-
-architecture BEHAVE of apply is                              signal BB_CONVOLVE_READY_OUT_0 : std_logic;
-    signal BB_CONVOLVE_VALID_OUT_0 : std_logic;
-    signal smooth : std_logic_vector(31 downto 0);                      begin                          
-VhdlComponent : entity work.Convolve                       
-    generic map(FILTERMATRIX => (1, 2, 1, 2, 4, 2, 1, 2, 1),
-                FILTER_SCALE => 16,
-                IMG_WIDTH => 640,
-                IMG_HEIGHT => 480)                       
-    port map(CLK => CLK,
-             RST => RST,
-             VALID_IN => VALID_IN,
-             READY_IN => READY_IN,
-             DATA_IN => img,
-             READY_OUT => BB_CONVOLVE_READY_OUT_0,
-             VALID_OUT => BB_CONVOLVE_VALID_OUT_0,
-             DATA_OUT => smooth); 
-
--- RETURN
-VALID_OUT <= BB_CONVOLVE_VALID_OUT_0;
-READY_OUT <= BB_CONVOLVE_READY_OUT_0;
-MODULE_OUT <= smooth;                      end BEHAVE;
