29c96c8f3898a753b115ada1198b3966b945f81f
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-06-27 21:39:48 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Sejits4Fpgas',
+    version='0.1',
+    packages=['sejits4fpgas.src'],
+    url='',
+    license='',
+    author='Philipp Ebensberger',
+    author_email='contact@3bricks-software.de',
+    description='',
+    requires=['numpy', 'pyserial', 'scikit-image']
+)
