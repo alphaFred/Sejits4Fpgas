acd3d97ad2eab08b9ff7137c470167ab34563506
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-06-30 20:26:44 +0200

    extended setup.py file with package information

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 setup(
     name='Sejits4Fpgas',
     version='0.1',
-    packages=['sejits4fpgas.src'],
+    packages=['sejits4fpgas'],
+    package_dir={'sejits4fpgas': 'sejits4fpgas'},
+    package_data={
+        '':['*.config']
+    },
     url='',
     license='',
     author='Philipp Ebensberger',
