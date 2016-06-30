8fe5cc53940b63720bfd51669e07b3a21e70b6f0
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-06-30 20:18:56 +0200

    removed config parser initialization

--- sejits4fpgas/src/utils.py
+++ sejits4fpgas/src/utils.py
@@ -1,32 +1,5 @@
 # ---------------------------------------------------------------------------
-# logging
-from os import path
-import logging
-
-logging.basicConfig(filename='vhdl_sejits.log', level=logging.DEBUG)
-LOG = logging.getLogger(__name__)
-LOG.info("initializing sejits_ctree")
-
-# ---------------------------------------------------------------------------
-try:
-    # python 2
-    import ConfigParser as configparser
-except ImportError:
-    # python 3
-    import configparser
-
-CONFIG = configparser.ConfigParser()
-DEFAULT_CFG_FILE_PATH = path.join(path.abspath(path.dirname(__file__)),
-                                  "defaults.cfg")
-
-LOG.info("reading default configuration from: %s", DEFAULT_CFG_FILE_PATH)
-
-CONFIG.readfp(open(DEFAULT_CFG_FILE_PATH), filename="defaults.cfg")
-
-# ---------------------------------------------------------------------------
 STDLIBS = ["ieee", "ieee.std_logic_1164.all"]
-
-
 # ---------------------------------------------------------------------------
 class TransformationError(Exception):
     """
