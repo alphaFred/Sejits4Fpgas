3ba0290ceebba16d47b31de0d8be94cf1e8cbfb4
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-07-05 23:10:22 +0200

    updated gitignore; updated setup.py to setuptools and implemented recoursive package_data addition

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('sejits4fpgas/hw')
+extra_files.append('*.config')
+
 
 setup(
     name='Sejits4Fpgas',
     version='0.1',
-    packages=['sejits4fpgas'],
+    packages=['sejits4fpgas', 'sejits4fpgas.src'],
     package_dir={'sejits4fpgas': 'sejits4fpgas'},
     package_data={
-        '':['*.config']
+        'sejits4fpgas':extra_files
     },
     url='',
     license='',
     author='Philipp Ebensberger',
     author_email='contact@3bricks-software.de',
     description='',
-    requires=['numpy', 'scikit-image', 'scipy', 'pytest']
+    install_requires=["numpy", "scikit-image", "scipy", "pytest"],
 )
