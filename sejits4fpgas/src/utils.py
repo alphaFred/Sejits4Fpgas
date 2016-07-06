b9dd9a680405e7529e881f3fbbbbe09b64348371
Philipp Ebensberger <ebensberger.philipp@googlemail.de>
2016-07-06 23:41:13 +0200

    refactored get basic block function; included configuration use

--- sejits4fpgas/src/utils.py
+++ sejits4fpgas/src/utils.py
@@ -1,5 +1,7 @@
 import glob
 import os
+from sejits4fpgas.src.config import config
+from pkg_resources import resource_filename
 
 from sejits4fpgas.src.nodes import VhdlFile
 
@@ -8,8 +10,9 @@ STDLIBS = ["ieee", "ieee.std_logic_1164.all"]
 # ---------------------------------------------------------------------------
 
 
-def get_basic_blocks(path):
+def get_basic_blocks():
     prebuilt_files = []
+    path = resource_filename("sejits4fpgas", config.get("Paths", "basic_block_path"))
     for fn in glob.glob(path + "*"):
         fname = os.path.basename(os.path.splitext(fn)[0])
         prebuilt_files.append(VhdlFile.from_prebuilt(name=fname, path=fn))
